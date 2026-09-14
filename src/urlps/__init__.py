a33222b40bf1d37c470e21482e4c39201e74fe16
Björn Schrammel <github@schrammel.dev>
2026-09-14 08:38:20 +0000

    chore(master): release 1.1.4

--- src/urlps/__init__.py
+++ src/urlps/__init__.py
@@ -68,7 +68,7 @@ Performance:
 from __future__ import annotations
 
 # x-release-please-start-version
-__version__ = "1.1.3"
+__version__ = "1.1.4"
 # x-release-please-end
 
 from ._audit import AuditCallback, AuditConfig, AuditEventCallback, AuditManager
